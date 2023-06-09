9bb0959ea2f500053673372af349b2e7a070fccf
Gauthan Bairi <gautham0647@gmail.com>
2023-06-09 12:25:28 +0530

    Refactor
    Refactor code in order summary page

--- my-app/src/pages/OrderSummary/OrderSummary.jsx
+++ my-app/src/pages/OrderSummary/OrderSummary.jsx
@@ -5,59 +5,9 @@ import { useCart } from "../../Context/CartContext";
 import { useAddress } from "../../Context/AddressContext";
 
 export const OrderSummary = () => {
-  // const { cart } = useCart();
-  const cart = [
-    {
-      _id: '9a857317-517d-48fb-92b4-62a034ca91e8',
-      productName: 'Jannatul Firdaus ',
-      productDescription: 'Jannatul Firdaus Concentrated Perfume Free From Alcohol 10ml For Unisex',
-      productImage: 'https://in.ajmalperfume.com/cdn/shop/products/Jannatul-Firadus1.png?v=1638013417',
-      categoryName: 'Contcentrated Perfumes',
-      price: 400,
-      discountPercent: 50,
-      originalPrice: 800,
-      onSale: true,
-      outOfStock: false,
-      rating: 4,
-      trending: true,
-      id: '1',
-      count: 1
-    }, {
-      _id: '276c6df5-1b0e-4a38-b3e0-8a26a42f44ba',
-      productName: 'Jimmy Choo Man',
-      productDescription: 'Jimmy Choo Man Blue Cologne 100 ml Eau De Toilette Spray',
-      productImage: 'https://img.fragrancex.com/images/products/parent/medium/76480m.webp',
-      categoryName: 'Contcentrated Perfumes',
-      price: 810,
-      discountPercent: 10,
-      originalPrice: 900,
-      onSale: true,
-      outOfStock: false,
-      rating: 1,
-      trending: true,
-      id: '6',
-      count: 1
-    },
-    {
-      _id: '02cbfc3b-99b4-4c9f-9440-0f7f0a0768af',
-      productName: 'Crystal Noir Perfume',
-      productDescription: '90 ml Eau De Parfum Spray',
-      productImage: 'https://img.fragrancex.com/images/products/parent/medium/60546w.webp',
-      categoryName: 'Contcentrated Perfumes',
-      price: 1188,
-      discountPercent: 5,
-      originalPrice: 1250,
-      onSale: true,
-      outOfStock: false,
-      rating: 3,
-      trending: true,
-      id: '7',
-      count: 1
-    },
-    
-  ]
+ const { cart } = useCart();
+  
 
-  console.log(cart)
   const { addressState } = useAddress();
   const { selectedAddress } = addressState;
   const totalOriginalPrice = cart.reduce(
